be22cd9cef0c5b6e7e166c375bce33fce40f39e5
Eric Moore <EricGittaMoore@duck.com>
2023-06-18 11:31:49 +0800

    feat: 首页分页

--- src/main/java/com/exam/supermarket/controller/IndexServlet.java
+++ src/main/java/com/exam/supermarket/controller/IndexServlet.java
@@ -1,7 +1,8 @@
 package com.exam.supermarket.controller;
 
 import com.exam.core.base.controller.BaseServlet;
-import com.exam.supermarket.po.GoodPo;
+import com.exam.core.common.metadata.IPage;
+import com.exam.core.common.plugin.pagination.Page;
 import com.exam.supermarket.service.GoodService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -17,7 +18,20 @@ public class IndexServlet extends BaseServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        goodService.list();
+        Integer page = 1;
+        Integer size = 10;
+        try {
+            page = req.getParameter("page") == null ? 1 : Integer.valueOf(req.getParameter("page"));
+            size = req.getParameter("size") == null ? 20 : Integer.valueOf(req.getParameter("size"));
+        } catch (NumberFormatException e) {
+            resp.sendRedirect(req.getServletPath());
+            return;
+        }
+
+        IPage pagination = new Page(page, Math.min(20, size));
+        goodService.page(pagination);
+        req.setAttribute("pagination", pagination);
+        req.setAttribute("goods", pagination.getRecords());
         req.getRequestDispatcher("/WEB-INF/templates/index.jsp").forward(req, resp);
     }
 }
